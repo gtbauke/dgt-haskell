e8d31e4c64151e118ec123d67b58b2d00c547b85
Gustavo Bauke <baukegustavo@gmail.com>
2024-03-04 12:08:36 +0000

    reads and prints files

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,18 @@
 module Main (main) where
 
+import System.Environment (getArgs)
 import Lib (createParserState, parseChar, parseString, runParser)
 
 main :: IO ()
 main = do
+  args <- getArgs
+  case args of
+    [fileName] -> do
+      putStrLn $ "The file name is: " ++ fileName
+      putStrLn $ readFile fileName
+    _ -> do
+      putStrLn "Usage: dtg <filename>"
+
   let state = createParserState "hellow"
   let result = runParser (parseString "hello") state
 
